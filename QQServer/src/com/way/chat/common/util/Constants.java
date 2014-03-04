147bfc150b95d775c2bc0319d99447f9dbe32880
unknown <waibao@ZTE9-PC-00174.sh.the9.com>
2014-03-04 18:24:21 +0800

    first

---
+++ QQServer/src/com/way/chat/common/util/Constants.java
@@ -0,0 +1,10 @@
+﻿package com.way.chat.common.util;
+
+public class Constants {
+	public static final String SERVER_IP = "172.18.13.28";// ������IP
+	public static final int SERVER_PORT = 8089;// �������˿�
+
+	public static final int CLIENT_SERVER_PORT = 8080;// �ͻ��˷������˿�
+	public static final int CLIENT_FILE_TRANSPORT_PORT = 8080;// �ͻ��˽����ļ��˿�
+	public static final int REGISTER_FAIL = 0;
+}
