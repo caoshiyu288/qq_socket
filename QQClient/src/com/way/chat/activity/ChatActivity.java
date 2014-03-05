30098f55f358d4d27e34c6df40ed5ab93506890c
unknown <waibao@ZTE9-PC-00174.sh.the9.com>
2014-03-05 09:59:44 +0800

    testgit status

--- QQClient/src/com/way/chat/activity/ChatActivity.java
+++ QQClient/src/com/way/chat/activity/ChatActivity.java
@@ -1,9 +1,8 @@
-package com.way.chat.activity;
+﻿package com.way.chat.activity;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import com.way.chat.common.bean.TextMessage;
 import com.way.chat.common.bean.User;
 import com.way.chat.common.tran.bean.TranObject;
